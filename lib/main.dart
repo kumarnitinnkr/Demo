009f50ec2a8155ecf8270748d233dfd8356eba9f
Nitin kumar <kumarnitinnkr@gmail.com>
2025-09-09 17:32:48 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My card',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('data'),
+          
+        ),
+       backgroundColor: Color(0xFFE0F7FA),  // Light cyan color
+
+      ),
+    );
+    
+  }
+}
